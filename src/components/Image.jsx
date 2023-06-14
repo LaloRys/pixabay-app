05934356fefdf129bc813009f5cefeaa79ae26b3
= <=>
2023-06-14 14:41:49 -0600

    quality of images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -7,12 +7,12 @@ const Image = (props) => {
    return (
       <div className='col-12 col-sm-6 col-md-4 col-lg-3 mb-4'>
          <div className='card'>
-            <img src={previewURL} alt={tags} className='card-img-top' />
+            <img src={webformatURL} alt={tags} className='card-img-top' />
             <div className='card-body'>
                <p className='card-text fw-light'>{likes} Me Gusta</p>
                <p className='card-text fw-light'>{views} Vistas</p>
 
-               <a href={webformatURL} target='_blank' className='btn btn-info d-block'> Ver imagen</a>
+               <a href={largeImageURL} target='_blank' className='btn btn-info d-block'> Ver imagen</a>
             </div>
          </div>
       </div>
